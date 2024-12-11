d14925985ff460fd3387911e6c1042ac9005016f
Xiang <liu.yx@berkeley.edu>
2024-12-10 17:39:35 -0800

    prefilter and kalman filter completed, full version of VWReg completed, turned out to perform really well in Chessboard scenario

--- Ego_motion.py
+++ Ego_motion.py
@@ -7,26 +7,94 @@ def WVReg(good_old, flow, h, f):
     return the result in the order of V_tran, V_long, and w"""
     Vx, Vy, x, y = flow[:, 0], flow[:, 1], good_old[:, 0], good_old[:, 1]
     # prepare the data points matrix
-    tran_data = - y / h
-    long_data = x * y / (f * h)
-    w_data = f + x ** 2 / f
-    tran_data, long_data, w_data = tran_data.reshape(tran_data.shape[0], 1), long_data.reshape(long_data.shape[0], 1), w_data.reshape(w_data.shape[0], 1)
-    X_data = np.concatenate((long_data, w_data), axis = 1)
-    
+    X_tran_data = - y / h
+    X_long_data = x * y / (f * h)
+    X_w_data = f + x ** 2 / f
+    Y_long_data = y ** 2 / (f * h)
+    Y_w_data = (x * y) / f
+
+    X_tran_data, X_long_data, X_w_data = X_tran_data.reshape(X_tran_data.shape[0], 1), X_long_data.reshape(X_long_data.shape[0], 1), X_w_data.reshape(X_w_data.shape[0], 1)
+    Y_long_data, Y_w_data = Y_long_data.reshape(Y_long_data.shape[0], 1), Y_w_data.reshape(Y_w_data.shape[0], 1)
+    X_data = np.concatenate((X_long_data, X_w_data), axis = 1)
+    Y_data = np.concatenate((Y_long_data, Y_w_data), axis = 1)
+
     res_X = np.linalg.lstsq(X_data, Vx)
+    res_Y = np.linalg.lstsq(Y_data, Vy)
 
-    return res_X[0][0], - res_X[0][1] # we should have a negative sign because our model takes downward as positive y-direction
+    V_long_X, w_X, Error_X = res_X[0][0], - res_X[0][1], res_X[1][0] # we should have a negative sign for the angular velocity because our model takes downward as positive y-direction
+    V_long_Y, w_Y, Error_Y = res_Y[0][0], - res_Y[0][1], res_Y[1][0]
 
-def Kalman_filter(op_Xs, cur_X, eror):
+    V_long, w, error = (V_long_X + V_long_Y) / 2, (w_X + w_Y) / 2, (Error_X + Error_Y) / 2
+    return  V_long, w, error
+
+def Kalman_filter(op_Xs, op_errors, cur_X, error):
     """key idea:
     the velocities of the car wouldn't change too much in a very short time, so the past velocities are somehow informative for our estimation of the current velocity
     combine our current estimation of the car's velocity with our knowledge of it's past velocity
     @ op_Xs: a time sequence of past estimations
+    @ op_errors: the errors in the past history
     @ cur_X: our current estimation
     @ error: the error of our current estimation"""
+    past_amplifier = 1.0 # past informations may have some errors regarding the current time no matter what, amplify the error given by the past time measurement by this factor
+    past_steps = 5 # the number of past steps that we want to consider
+    past_len = len(op_Xs)
 
-    past_steps = 10 # the number of past steps that we want to consider
+    if past_len == 0:
+        return cur_X # if we are at the very first step, return the original estimation directly, there is no way to use past information for estimation
+    
+    past_steps = min(past_steps, past_len)
+    past_Xs, past_Errors = op_Xs[- past_steps : ], op_errors[- past_steps : ]
 
+    # now take the average of past measurements and past errors
+    past_X, past_Error = np.average(past_Xs), np.average(past_Errors)
+    past_Error *= past_amplifier
+
+    Kalman_gain = error / (error + past_Error)
+    res_X = cur_X + Kalman_gain * (past_X - cur_X)
+    return res_X
+
+def pre_filter(good_old, flow, h, f, op_Vl, op_w):
+    """key idea:
+    use the information from past time (op_Vl, op_w) to filter out obvious outliers in the data, since the velocities of the car cannot change too much in very limited time
+    for a point with Vx, Vy, and pre_Vx, pre_Vy
+    its PREFACTOR = abs((Vx - pre_Vx) / pre_Vx ) + abs((Vy - pre_Vy) / pre_Vy)
+
+    @ return: flow vector without obvious outliers"""
+    discard_threshold = 3.0 # the threshold for discard a point, if a point's preFactor is higher than this threshold, discard it
+
+    past_steps = 5  # the length of the horizon of how long we want to look back
+    past_len = len(op_Vl)
+
+    if past_len == 0:
+        return good_old, flow # if we are at the very start, this function is not applicable, return good_old, and flow directly
+    
+    # obtain past information
+    past_steps = min(past_len, past_steps)
+    past_Vl, past_w = np.average(op_Vl[- past_steps : ]), np.average(op_w[- past_steps : ])
+    pre_VW = np.asarray([[past_Vl], [past_w]])
+
+    Vx, Vy, x, y = flow[:, 0], flow[:, 1], good_old[:, 0], good_old[:, 1]
+
+    # prepare the data points matrix
+    X_tran_data = - y / h
+    X_long_data = x * y / (f * h)
+    X_w_data = f + x ** 2 / f
+    Y_long_data = y ** 2 / (f * h)
+    Y_w_data = (x * y) / f
+
+    X_tran_data, X_long_data, X_w_data = X_tran_data.reshape(X_tran_data.shape[0], 1), X_long_data.reshape(X_long_data.shape[0], 1), X_w_data.reshape(X_w_data.shape[0], 1)
+    Y_long_data, Y_w_data = Y_long_data.reshape(Y_long_data.shape[0], 1), Y_w_data.reshape(Y_w_data.shape[0], 1)
+    X_data = np.concatenate((X_long_data, X_w_data), axis = 1)
+    Y_data = np.concatenate((Y_long_data, Y_w_data), axis = 1)
+
+    # use past information to give an approximation about Vx and Vy
+    Vx, Vy = Vx.reshape(Vx.shape[0], 1), Vy.reshape(Vy.shape[0], 1)
+    pre_Vx, pre_Vy = np.matmul(X_data, pre_VW), np.matmul(Y_data, pre_VW)
+    prefactors = np.abs((Vx - pre_Vx) / pre_Vx ) + np.abs((Vy - pre_Vy) / pre_Vy)
+    filter_mask = prefactors <= discard_threshold
+    filter_mask = filter_mask.reshape(filter_mask.shape[0])
+
+    return good_old[filter_mask], flow[filter_mask]
 
 def test():
     start_frame = 0
--- test.py
+++ test.py
@@ -6,31 +6,47 @@ import matplotlib.pyplot as plt
 
 def raw_test():
     images, real_Vl, real_w, f, h, deltaT = du.full_parse()
-    op_Vt, op_Vl, op_w = [], [], []
-
-    start_frame = 100
-    end_frame = 400
+    op_Vt, op_Vl, op_w = [], [], [] # all values estiamted by pure optical flow
+    est_Vt, est_Vl, est_w = [], [], [] # values that are optimized by Kalman filter and other algorithms
+    Errors = [] # record the errors of past estimations
+    start_frame = 20
+    end_frame = 500
 
     for i in range(start_frame, end_frame):
         preImg, nextImg = images[i], images[i + 1]
         mask = pu.G_cutoff(preImg)
         good_old, good_new, flow = pu.cv_featureLK(preImg, nextImg, deltaT, mask)
-        V_long, w = em.WVReg(good_old, flow, h, f)
+
+        good_old, flow = em.pre_filter(good_old, flow, h, f, op_Vl, op_w)
+
+        V_long, w, Error = em.WVReg(good_old, flow, h, f) # use egomotion estimation function to estimate the ego motion
+
+        # apply optimization algorithms here
+        final_V_long = em.Kalman_filter(op_Vl, Errors, V_long, Error)
+        final_w = em.Kalman_filter(op_w, Errors, w, Error)
 
         op_Vl.append(V_long)
         op_w.append(w)
+        est_Vl.append(final_V_long)
+        est_w.append(final_w)
+        Errors.append(Error)
 
         # have some printing imformation every 7 frames
         if i % 7 == 0:
-            print(f"op_V_long = {V_long}, op_w = {w}, real_V_long = {real_Vl[i]}, real_w = {real_w[i]}")
+            print(f"""///////////////////// at frame {i} ////////////////////
+                  op_V_long = {V_long}, op_w = {w}, 
+                  est_V_long = {final_V_long}, est_w = {final_w},
+                  real_V_long = {real_Vl[i]}, real_w = {real_w[i]}""")
 
     f, (ax1, ax2, ax3) = plt.subplots(1, 3)
     # ax1.plot(real_Vt[start_frame : end_frame], label = "real_V_tran")
     # ax1.plot(op_Vt, label = "op_V_tran")
     ax2.plot(real_Vl[start_frame : end_frame], label = "real_V_long")
-    ax2.plot(op_Vl, label = "op_V_long")
+    # ax2.plot(op_Vl, label = "op_V_long")
+    ax2.plot(est_Vl, label = "est_V_long")
     ax3.plot(real_w[start_frame : end_frame], label = "real_w")
-    ax3.plot(op_w, label = "op_w")
+    # ax3.plot(op_w, label = "op_w")
+    ax3.plot(est_w, label = "est_w")
     ax1.legend()
     ax2.legend()
     ax3.legend()
